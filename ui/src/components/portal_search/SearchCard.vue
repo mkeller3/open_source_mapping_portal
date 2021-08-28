d418a90470b4bba4b723c393e8b717da1ceaa409
Michael Keller <michaelkeller03@gmail.com>
2021-08-28 06:53:08 -0500

    Updates to try and add okteto settings

--- ui/src/components/portal_search/SearchCard.vue
+++ ui/src/components/portal_search/SearchCard.vue
@@ -3,14 +3,19 @@
     <v-list-item three-line>
       <v-list-item-avatar tile size="100" color="grey"></v-list-item-avatar>
       <v-list-item-content>
-        <a :href="'/portal_item/?id='+result.id+'&type='+result.slug" target="_blank"
+        <a
+          :href="'/portal_item/?id=' + result.id + '&type=' + result.slug"
+          target="_blank"
           ><v-list-item-title class="text-h5 mb-1">
             {{ result.title }}
           </v-list-item-title></a
         >
-        <div class="text-overline mb-4">{{ result.description }}</div>
         <v-list-item-subtitle
-          >Created By:{{ result.created_by }} Updated:
+          >{{result.type}} by {{ result.created_by }}</v-list-item-subtitle
+        >
+        <div class="">{{ result.description }}</div>
+        <v-list-item-subtitle
+          >Updated:
           {{ result.updated_date }}</v-list-item-subtitle
         >
       </v-list-item-content>
